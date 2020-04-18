4badd5e45daf1622b9a9fb65e7fc3783080e51b3
Jonathan Marshall <Jonathan@marshalltech.co.uk>
2020-04-18 15:44:30 +0100

    swap <i> with fontAwesomeIcons fix style syntax

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ class Navigation extends Component {
                     <li className="mr-6 my-2 md:my-0">
                         <a href="/"
                            className="block py-1 md:py-3 pl-1 align-middle active-here border-b-2 text-blue-400 border-blue-400 not-active-here text-gray-400 no-underline hover:text-gray-100 hover:border-blue-400">
-                            <FontAwesomeIcon icon="check-square" />
+                            <FontAwesomeIcon icon="home" className="mr-3 text-blue-400" />
                             <span className="pb-1 md:pb-0 text-sm">Home</span>
                         </a>
                     </li>
@@ -20,36 +20,37 @@ class Navigation extends Component {
                         <a href="/users"
                            className="block py-1 md:py-3 pl-1 align-middle active-here 'border-b-2 text-blue-400 border-pink-400 not-active-here text-gray-400 no-underline hover:text-gray-100 hover:border-pink-400">
                             <i className="fas fa-tasks fa-fw mr-3"/>
+                            <FontAwesomeIcon icon="tasks" className="mr-3 text-blue-400" />
                             <span className="pb-1 md:pb-0 text-sm">Users</span>
                         </a>
                     </li>
                     <li className="mr-6 my-2 md:my-0">
                         <a href="/orders"
                            className="block py-1 md:py-3 pl-1 align-middle active-here  'border-b-2 text-blue-400 border-purple-400 not-active-here text-gray-400 no-underline hover:text-gray-100 hover:border-purple-400">
-                            <i className="fa fa-envelope fa-fw mr-3"/>
+                            <FontAwesomeIcon icon="envelope" className="mr-3 text-blue-400" />
                             <span className="pb-1 md:pb-0 text-sm">Orders</span>
                         </a>
                     </li>
                     <li className="mr-6 my-2 md:my-0">
                         <a href="/machines"
                            className="block py-1 md:py-3 pl-1 align-middle active-here  ? 'border-b-2 text-blue-400 border-green-400 not-active-here text-gray-400 no-underline hover:text-gray-100 hover:border-green-400">
-                            <i className="fas fa-chart-area fa-fw mr-3"/>
+                            <FontAwesomeIcon icon="chart-area" className="mr-3 text-blue-400" />
                             <span className="pb-1 md:pb-0 text-sm">Machines</span>
                         </a>
                     </li>
                     <li className="mr-6 my-2 md:my-0">
                         <a href="/logs"
                            className="block py-1 md:py-3 pl-1 align-middle active-here border-b-2 text-blue-400 border-red-400 not-active-here text-gray-400 no-underline hover:text-gray-100 hover:border-red-400">
-                            <i className="fa fa-wallet fa-fw mr-3"/>
+                            <FontAwesomeIcon icon="wallet" className="mr-3 text-blue-400" />
                             <span className="pb-1 md:pb-0 text-sm">Logs</span>
                         </a>
                     </li>
                 </ul>
 
-                <div className="relative pull-right pl-4 pr-4 md:pr-0" style="z-index: 100000;">
+                <div className="relative pull-right pl-4 pr-4 md:pr-0" style={{"z-index": "100000"}}>
                     <input type="search" placeholder="Search"
                            className="w-full bg-gray-900 text-sm text-gray-400 transition border border-gray-800 focus:outline-none focus:border-gray-600 rounded py-1 px-2 pl-10 appearance-none leading-normal">
-                        <div className="absolute search-icon" style="top: 0.375rem;left: 1.75rem;">
+                        <div className="absolute search-icon" style={{"top": "0.375rem", "left": "1.75rem"}}>
                             <svg className="fill-current pointer-events-none text-gray-500 w-4 h-4"
                                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
@@ -97,7 +98,7 @@ class Navigation extends Component {
                         <div className="w-1/2 pl-2 md:pl-0">
                             <a className="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold"
                                href="/">
-                                <i className="fas fa-moon text-blue-400 pr-3"/>
+                                <FontAwesomeIcon icon="file-medical-alt" className="mr-3 text-blue-400"/> Filezone
                             </a>
                         </div>
 
